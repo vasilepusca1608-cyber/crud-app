eaf6ea43447f9991a3486aebeaed7eaf2fa64e2a
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-06 19:56:25 +0200

    Run Application

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -1,7 +1,9 @@
 package pixel.academy.crud_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -10,4 +12,12 @@ public class CrudAppApplication {
 		SpringApplication.run(CrudAppApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello, world!");
+		};
+	}
 }
