28445c3478a2b3c53604d0d455ca065165e04a34
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-10 11:21:45 +0200

    New interfes and class

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pixel.academy.crud_app.dao.StudentDAO;
+import pixel.academy.crud_app.entity.Student;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -12,12 +14,12 @@ public class CrudAppApplication {
 		SpringApplication.run(CrudAppApplication.class, args);
 	}
 
-
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
-
-		return runner -> {
-			System.out.println("Hello, world!");
-		};
+		@Bean
+				public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+			return  runner-> {
+				createStudent(studentDAO);
+			}
+		}
+		private void createStudent(StudentDAO studentDAO){
 	}
 }
