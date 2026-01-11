61d5e6350d2df7f50859c2cce3e43d877659b570
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-11 13:55:15 +0200

    we call the delete method

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -25,9 +25,16 @@ public class CrudAppApplication {
 			//readStudent(studentDAO);
 			//queryForStudents(studentDAO);
 			//queryForStudentsByLastName(studentDAO);
-			updateStudent(studentDAO);
+			//updateStudent(studentDAO);
+			deleteStudent(studentDAO);
 
 		};
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+		int studentId = 3;
+		System.out.println("Deleting student id: " + studentId);
+		studentDAO.delete(studentId);
 	}
 
 	private void updateStudent(StudentDAO studentDAO) {
